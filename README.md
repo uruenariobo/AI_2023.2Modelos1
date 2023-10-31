e997b09a46b163807be29db65709dea47522a306
Miguel Urueña <cayu2312002@gmail.com>
2023-10-30 22:35:45 -0500

    Phase 2: Container Deployment and Model Training.

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # AI_2023.2Modelos1
+
+# NYC Taxi Fare Prediction Challenge
+
+Welcome to the NYC Taxi Fare Prediction Challenge repository! This project is part of the competition hosted on Kaggle, where the goal is to predict taxi fare amounts in New York City based on various features.
+
+## Competition Overview
+
+The [New York City Taxi Fare Prediction Challenge](https://www.kaggle.com/competitions/new-york-city-taxi-fare-prediction/overview) aims to improve the accuracy of fare amount predictions for taxi rides in NYC using machine learning techniques. The primary evaluation metric is the Root Mean-Squared Error (RMSE), where smaller values indicate better predictive performance.
+
+The challenge is to develop predictive models that can estimate taxi fare amounts more accurately than basic distance-based estimates.
+
+## Project Objectives
+
+In this project, we will develop and deploy a predictive model to estimate taxi fare amounts. The first phase focuses on building the prediction model, and the Jupyter Notebook file for this phase is located in the `fase-1` directory. Below are the steps to execute Phase 1 successfully:
+
+## Phase 1: Prediction Model
+
+1. **Clone the Repository:**
+   - Start by cloning this repository to your local environment using Git. Open your terminal and run the following command:
+
+     ```bash
+     git clone https://github.com/uruenariobo/AI_2023.2Modelos1.git
+     ```
+
+2. **Download Python 3.11:**
+   - Make sure you have Python 3.11 installed on your system. You can download it from the official Python website [here](https://www.python.org/downloads/release/python-3110/).
+
+3. **Set Up a Virtual Environment (Optional):**
+   - To manage Python dependencies, consider creating a virtual environment. You can create one using the following commands:
+
+     ```bash
+     python3 -m venv venv
+     source venv/bin/activate  # On Windows, use "venv\Scripts\activate"
+     ```
+
+4. **Open the Project in Visual Studio Code:**
+   - If you haven't already, download and install Visual Studio Code (VS Code) from the official website: [Visual Studio Code](https://code.visualstudio.com/).
+
+   - Launch VS Code and open the project folder. You can do this by selecting "File" > "Open Folder" and selecting the folder where you cloned the repository.
+
+5. **Execute Python Code:**
+   - To run Python scripts in VS Code, open the Jupyter Notebook file `fase-1/nyc-taxi-fares-eda-modelling-2-93.ipynb`. You can execute code cells in the notebook individually by selecting the cell and clicking the "Run Cell" button (▶️) or using the keyboard shortcut (Shift+Enter).
+
+   - Ensure that your Python environment is correctly configured, and the required packages are installed.
+
+## Participants
+
+- Miguel Angel Urueña Riobo
+  - Email: miguel.uruena@udea.edu.co
+  - Program: Ingeniería de Sistemas
+
+## Dataset
+
+You can access the competition dataset [here](https://www.kaggle.com/competitions/new-york-city-taxi-fare-prediction/data).
+
+## Next Phases
+
+Stay tuned for subsequent phases of this project, where we will deploy the model in a container and create a RESTful API for predictions.
+
+We hope you enjoy participating in this project and challenge. Happy modeling!
---
+++ fase-2/train.py
@@ -0,0 +1,144 @@
+# Importar las bibliotecas necesarias
+import argparse  # Para manejar argumentos de la línea de comandos
+import pickle  # Para serializar/deserializar modelos
+import pandas as pd  # Para trabajar con datos en formato tabular
+from sklearn.model_selection import train_test_split  # Para dividir los datos en conjuntos de entrenamiento y prueba
+from sklearn.neighbors import KNeighborsRegressor  # Para el modelo KNN
+from sklearn.metrics import mean_squared_error  # Para calcular la métrica RMSE
+import numpy as np  # Para operaciones matemáticas
+from datetime import datetime  # Para trabajar con fechas y horas
+
+# Definir los valores por defecto para los argumentos
+DEFAULT_TRAIN_FILE = 'train.csv'  # Nombre del archivo CSV de datos de entrenamiento
+DEFAULT_MODEL_FILE = 'model.pkl'  # Nombre del archivo para guardar el modelo entrenado
+
+# Configurar los argumentos de la línea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_file', type=str, default=DEFAULT_TRAIN_FILE, help='a CSV file with train data')  # Argumento para el archivo de datos de entrenamiento
+parser.add_argument('--model_file', type=str, default=DEFAULT_MODEL_FILE, help='where the trained model will be stored')  # Argumento para el archivo de modelo entrenado
+
+args = parser.parse_args()  # Analizar los argumentos de la línea de comandos
+train_file = args.train_file  # Nombre del archivo de datos de entrenamiento
+model_file_arg = args.model_file  # Nombre del archivo para guardar el modelo entrenado (renombrado para evitar conflictos)
+
+# Función para limpiar el DataFrame de datos
+def clean_df(df):
+    """
+    Limpia el DataFrame de datos de entrenamiento.
+
+    Parameters:
+    df (pd.DataFrame): El DataFrame de datos de entrenamiento.
+
+    Returns:
+    pd.DataFrame: El DataFrame de datos limpios.
+    """
+    # Selecciona solo los primeros 15 caracteres de la columna 'pickup_datetime'
+    df['pickup_datetime'] = df['pickup_datetime'].str.slice(0, 15)
+    # Convierte la columna 'pickup_datetime' a formato datetime en UTC
+    df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'], utc=True, format='%Y-%m-%d %H:%M')
+    
+    # Corrige incorrectamente las coordenadas de longitud/latitud
+    df = df.assign(rev=df.dropoff_latitude < df.dropoff_longitude)
+    idx = (df['rev'] == 1)
+    df.loc[idx, ['dropoff_longitude', 'dropoff_latitude']] = df.loc[idx, ['dropoff_latitude', 'dropoff_longitude']].values
+    df.loc[idx, ['pickup_longitude', 'pickup_latitude']] = df.loc[idx, ['pickup_latitude', 'pickup_longitude']].values
+    
+    # Filtra y limpia los puntos de datos fuera de los rangos apropiados
+    criteria = (
+        " 0 < fare_amount <= 500"
+        " and 0 < passenger_count <= 6 "
+        " and -75 <= pickup_longitude <= -72 "
+        " and -75 <= dropoff_longitude <= -72 "
+        " and 40 <= pickup_latitude <= 42 "
+        " and 40 <= dropoff_latitude <= 42 "
+    )
+    df = (df
+          .dropna()
+          .query(criteria)
+          .reset_index()
+          .drop(columns=['rev', 'index'])
+         )
+    return df
+
+# Cargar los datos de entrenamiento desde un archivo CSV
+def load_data(file_path):
+    """
+    Carga los datos de entrenamiento desde un archivo CSV y aplica la limpieza.
+
+    Args:
+        file_path (str): Ruta al archivo CSV que contiene los datos de entrenamiento.
+
+    Returns:
+        pandas.DataFrame: El DataFrame de datos limpios y procesados.
+    """
+    cols = ['fare_amount', 'pickup_datetime', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'passenger_count']
+    train = pd.read_csv(file_path, usecols=cols)
+    # Realiza la limpieza de los datos
+    train = clean_df(train)
+    return train
+
+# Modelo K-Nearest Neighbors (KNN)
+def knn_model(x_train, x_test, y_train, y_test, neighbors):
+    """
+    Entrena un modelo K-Nearest Neighbors (KNN) y realiza predicciones.
+
+    Args:
+        x_train (pandas.DataFrame): Conjunto de entrenamiento de características.
+        x_test (pandas.DataFrame): Conjunto de prueba de características.
+        y_train (pandas.Series): Etiquetas de entrenamiento.
+        y_test (pandas.Series): Etiquetas de prueba.
+        neighbors (list of int): Lista de números de vecinos para probar.
+
+    Returns:
+        tuple: Un tuple que contiene el modelo KNN entrenado, el RMSE mínimo y las mejores predicciones.
+    """
+    min_rmse = 1000
+
+    # Convierte la columna 'pickup_datetime' a valores numéricos
+    x_train['pickup_datetime'] = (x_train['pickup_datetime'].dt.tz_localize(None) - datetime(1970, 1, 1)).dt.total_seconds().astype(float)
+    x_test['pickup_datetime'] = (x_test['pickup_datetime'].dt.tz_localize(None) - datetime(1970, 1, 1)).dt.total_seconds().astype(float)
+
+    for n in neighbors:
+        knn = KNeighborsRegressor(n_neighbors=n)
+        knn.fit(x_train, y_train)
+        pred = knn.predict(x_test)
+        rmse = np.sqrt(mean_squared_error(y_test, pred))
+        if rmse < min_rmse:
+            min_rmse = rmse
+            model = knn
+            best_pred = pred
+        print('Neighbours', n, 'RMSE', rmse)
+    return model, min_rmse, best_pred
+
+# Construir el modelo y realizar el entrenamiento con KNN
+def build_and_train_model(train, model_fn):
+    """
+    Construye un modelo y realiza el entrenamiento con KNN.
+
+    Args:
+        train (pandas.DataFrame): DataFrame de datos de entrenamiento.
+        model_fn (function): Función que entrena un modelo KNN.
+
+    Returns:
+        None
+    """
+    X = train.drop(columns=['fare_amount'])
+    y = train['fare_amount']
+
+    # Divide los datos en conjuntos de entrenamiento y prueba
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Especifica una lista de números de vecinos para probar
+    neighbors = [10, 20]
+
+    # Entrenamiento de modelos KNN con diferentes números de vecinos
+    model, rmse, pred = model_fn(x_train, x_test, y_train, y_test, neighbors)
+
+    # Guardar el modelo entrenado en el archivo especificado
+    with open(model_file_arg, 'wb') as model_file:
+        pickle.dump(model, model_file)
+
+if __name__ == "__main__":
+    train_data = load_data(train_file)  # Cargar los datos de entrenamiento desde el archivo CSV
+    # Llamar a la función build_and_train_model para entrenar el modelo KNN
+    build_and_train_model(train_data, knn_model)
